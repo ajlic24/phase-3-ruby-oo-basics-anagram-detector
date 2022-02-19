cc50fab1721c8301c21f8d6fff11b4e1fe18f638
ajlic24@gmail.com <ajlic24@gmail.com>
2022-02-19 18:03:56 -0500

    commit tree 6ede059d856a69a0e48da5a0235811d5bee29944

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        # @array = array
+        ans = []
+        to_compare = @word.chars.sort
+        array_to_compare = array.filter do |word|
+            word.chars.sort === to_compare
+        end
+    end
+
+    # def match
+    #     @array_to_compare
+    # end
+
+end
+# binding.pry
+# example = Anagram.new('dark')
+
+# example.match(['yellow', 'white', 'dark', 'blue'])
+
+# pp example.match
